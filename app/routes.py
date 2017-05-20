b6eeb4341a7d51bc346e8360ce91d37b23e085ca
STEFAN BAATZ <waltz.for.zizi@gmail.com>
2017-05-19 18:08:11 -0700

    Added index trucks, show, new form, edit form, and post routes!

--- app/routes.py
+++ app/routes.py
@@ -8,65 +8,56 @@ class MonsterTruck (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     color = db.Column(db.String(25))
-    driver_name = db.Column(db.String(50))
 
-
-    def __init__(self, name, color, driver_name):
+    def __init__(self, name, color):
         self.name = name
         self.color = color
-        self.driver_name = driver_name
 
     def __repr__(self):
         return '<MonsterTruck %r>' % self.name
 
+#GET HOMEPAGE
+@app.route('/')
+def home ():
+    return render_template('home.html')
 
+#GET ALL TRUCKS
+@app.route('/monster_trucks')
+def index ():
+    trucks = MonsterTruck.query.all()
+    return render_template('index.html', trucks=trucks)
 
+#GET ONE TRUCK
+@app.route('/monster_trucks/<page_id>/')
+def show (page_id):
+    truck = MonsterTruck.query.filter_by(id=page_id)
+    return render_template('show.html', truck=truck)
 
+#GET NEW TRUCK FORM
+@app.route('/monster_trucks/new')
+def new ():
+    return render_template('new.html')
 
+#POST NEW TRUCK
+@app.route('/new', methods=['POST'])
+def postTruck ():
+    newTruck = MonsterTruck(request.form['name'], request.form['color'])
+    db.session.add(newTruck)
+    db.session.commit()
+    return redirect('/monster_trucks')
 
-# from sqlalchemy import create_engine
-# from sqlalchemy import Column, String
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-#
-# @app.route('/')
-# @app.route('/index')
-# def index():
-#     user = { 'name': 'Stefan' }
-#     return jsonify(**user)
-#
-#
-# db_string = "postgresql://localhost/python_monster_trucks"
-#
-# db = create_engine(db_string)
-# base = declarative_base()
-#
-# class MonsterTruck (base):
-#     __tablename__ = 'monster_trucks'
-#
-#     name = Column(String, primary_key=True)
-#     color = Column(String)
-#     driver_name = Column(String)
-#
-# Session = sessionmaker(db)
-# session = Session()
-#
-# base.metadata.create_all(db)
-#
-# # Create
-# newTruck = MonsterTruck(name="Doctor Crush", color="Orange", driver_name="Bobus")
-# session.add(newTruck)
-# session.commit()
-#
-# # Read
-# films = session.query(Film)
-# for film in films:
-#     print(film.title)
-#
-# # Update
-# doctor_strange.title = "Some2016Film"
-# session.commit()
-#
-# # Delete
-# session.delete(doctor_strange)
-# session.commit()
+#GET EDIT TRUCK FORM
+@app.route('/monster_trucks/<page_id>/edit')
+def edit (page_id):
+    truck = MonsterTruck.query.filter_by(id=page_id)
+    return render_template('edit.html', truck=truck)
+
+#UPDATE ONE TRUCK
+@app.route('/new', methods=['PUT'])
+def putTruck ():
+    newTruck = MonsterTruck(request.form['name'], request.form['color'])
+    db.session.add(newTruck)
+    db.session.commit()
+    return redirect('/monster_trucks')
+
+#DELETE ONE TRUCK
